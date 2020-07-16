f9405ecc5815f73e6dce0c9cfbf5fd64755c8ac3
gauravmanchanda99 <53062583+gauravmanchanda99@users.noreply.github.com>
2020-07-16 16:10:42 -0400

    Displaying Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,8 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("A Test Recipe", "This is a Sample of Recipe",
     "https://upload.wikimedia.org/wikipedia/commons/3/3f/Vegetarian_pizza.jpg"),
-    
+    new Recipe("A Test Recipe", "This is a Sample of Recipe",
+    "https://upload.wikimedia.org/wikipedia/commons/3/3f/Vegetarian_pizza.jpg")
   ];
   
   constructor() { }
